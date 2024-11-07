b239dd28be370157ad0f47e2d7539d6e374966fe
Lilaliz1 <lilaliz1515@gmail.com>
2024-11-06 21:23:15 -0600

    Firebase

---
+++ Tienda_v1/src/main/java/com/tienda/service/FirebaseStorageService.java
@@ -0,0 +1,23 @@
+
+package com.tienda.service;
+
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+@Service
+public interface FirebaseStorageService {
+
+    public String cargaImagen(MultipartFile archivoLocalCliente, String carpeta, Long id);
+
+    //El BuketName es el <id_del_proyecto> + ".appspot.com"
+    final String BucketName = "tecshop-13ca5.appspot.com";
+
+    //Esta es la ruta básica de este proyecto Techshop
+    final String rutaSuperiorStorage = "techshop";
+
+    //Ubicación donde se encuentra el archivo de configuración Json
+    final String rutaJsonFile = "firebase";
+    
+    //El nombre del archivo Json
+    final String archivoJsonFile = "tecshop-13ca5-firebase-adminsdk-ks54h-2c19de0223.json";
+}
--- Tienda_v1/src/main/java/com/tienda/service/impl/CategoriaServiceImpl.java
+++ Tienda_v1/src/main/java/com/tienda/service/impl/CategoriaServiceImpl.java
@@ -1,38 +1,62 @@
+
+import com.tienda.service.CategoriaService;
+
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-
 /**
  *
  * @author Lizeth
  */
 public class CategoriaServiceImpl {
-    package com.tienda.service.impl;
 
-import com.tienda.dao.CategoriaDao;
-import com.tienda.domain.Categoria;
-import com.tienda.service.CategoriaService;
-import java.util.List;//
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;//service es un estereotipo en java, esta clase es de servicio
-import org.springframework.transaction.annotation.Transactional;
+    package com.tienda.service.impl ;
 
-@Service
-public class CategoriaServiceImpl implements CategoriaService {
-    
-    @Autowired// instancia en el service de la categoria dao del metodo findall, findbyid
-    private CategoriaDao categoriaDao;
+    import com.tienda.dao.CategoriaDao ;
+    import com.tienda.domain.Categoria ;
+    import com.tienda.service.CategoriaService ;
+    import java.util.List ;//
+    import org.springframework.beans.factory.annotation.Autowired ;
+    import org.springframework.stereotype.Service ;//service es un estereotipo en java, esta clase es de servicio
+    import org.springframework.transaction.annotation.Transactional ;
 
-    @Override
-    @Transactional(readOnly=true)//cuando estoy agregando los metodos de findall, o por id
-    public List<Categoria> getCategorias(boolean activos) {
-        var lista=categoriaDao.findAll();
-        if (activos) {
-           lista.removeIf(e -> !e.isActivo());//no mostrar las categorias no activas
+    @Service
+    public class CategoriaServiceImpl implements CategoriaService {
+
+        @Autowired// instancia en el service de la categoria dao del metodo findall, findbyid
+        private CategoriaDao categoriaDao;
+
+        @Override
+        @Transactional(readOnly = true)//cuando estoy agregando los metodos de findall, o por id
+        public List<Categoria> getCategorias(boolean activos) {
+            var lista = categoriaDao.findAll();
+            if (activos) {
+                lista.removeIf(e -> !e.isActivo());//no mostrar las categorias no activas
+            }
+            return lista;
         }
-        return lista;
+
+        @Override
+        @Transactional(readOnly = true)
+        public Categoria getCategoria(Categoria categoria) {
+            return categoriaDao.findBy(categoria.getIdCategoria()).orElse(null);
+
+        }
+
+        @Override
+        @Transactional
+        public void save(Categoria categoria)
+
+        {
+            categoriaDao.save(categoria);
+        }
+        @Override
+        @Transactional
+        public void delete(Categoria categoria){
+             categoriaDao.delete(categoria);
+        }
+
     }
-}
-    
+
 }
